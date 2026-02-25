f7a776c4bfee0d596c8dab081c283332a7a0bc81
avishaigonen123 <avishaigonen@gmail.com>
2026-02-25 10:02:43 +0200

    finished set and get

--- BitArray.cpp
+++ BitArray.cpp
@@ -21,153 +21,25 @@ BitArray::BitArray(const BitArray& copy) {
 }
 
 BitArray::~BitArray() {
-	delete[] this->storage;
+	this->size = 0;
+
+	delete this->storage;
 	this->storage = nullptr;
 }
 
-size_t BitArray::getSize() const
-{
-	return this->size;
-}
-
-void BitArray::resize(size_t new_capacity) {
-	if (new_capacity < capacity) // resizing can't work
-		return;
-
-	size_t* tmp;
-	tmp = this->storage;
-
-	this->storage = new size_t[new_capacity];
-	size_t i;
-	for (i = 0; i < capacity; i++)
-	{
-		this->storage[i] = tmp[i];
-	}
-	for (; i < new_capacity; i++)
-	{
-		this->storage[i] = '\0';
-	}
-	capacity = new_capacity;
-
-	delete[]tmp;
-	tmp = nullptr;
-}
-
-// - Answer: size_t becuase i don't want negetive value (and then might have heap underflow)
 void BitArray::SetAt(size_t index, bool value) {
+	// size_t becuase i don't want negetive value (and then might have heap underflow)
 	size_t bytes = index / 8;
 	size_t bits = index % 8;
+	char mask = !((bits << 1) & this->storage[bytes]); // I want mask, like 11101111
 
-	if (this->capacity * 8 < index) // we need more capacity
-		resize(bytes + 1);
-	if (this->size < index) // enlarge the size
-		this->size = index;
-
-	if (value) 
-		this->storage[bytes] = this->storage[bytes] | (bits ? (1 << bits) : 1); // OR 00010000
-	else
-		this->storage[bytes] = this->storage[bytes] & (bits ? ~(1 << bits) : 0xfe);// AND 11101111
+	this->storage[bytes] = (this->storage[bytes] & mask) + (bits << 1); // turn of the bit with inx, and add it after left shift. 
 }
 
-// - Answer: function is const becuase i don't want to change to values of the fields at "this", the class.
-size_t BitArray::GetAt(size_t index) const {
-	if (index > size)
-		return OutOfBound;
-	
+bool BitArray::GetAt(size_t index) const {
+	// function is const becuase i don't want to change to values of the fields at "this", the class.
 	size_t bytes = index / 8;
 	size_t bits = index % 8;
-
-	return (this->storage[bytes] >> (bits)) & 1; // take the right byte, shift right, and mask with 1 (to get the exact bit)
-}
-
-size_t BitArray::ToBinaryStr(char* o_binaryStr, size_t binaryStrSize) const {
-	if (binaryStrSize > this->size) // bigger than the size
-		return HeapOverflow;
-
-	char* ptr = o_binaryStr; // we'll move and add the characters. I assume a place had already been allocated at o_binaryStr
-	size_t* ptr_storage = this->storage;
-	size_t curr, i = 0;
-	while(i < binaryStrSize) {
-		curr = *ptr_storage;
-		for (int j = 0; j < 8 && i < binaryStrSize; i++, j++)
-		{
-			*ptr = ((curr >> j) & 1) ? '1' : '0';
-+			ptr++;
-		}
-		ptr_storage++;
-	}
-	*ptr = '\0';
-
-	return 0;
-}
-
-size_t BitArray::FromBinaryStr(const char* i_binaryStr, size_t binaryStrLen) {
-	if (binaryStrLen > this->size) // I assume binaryStrLen is without NULL
-		this->SetAt(binaryStrLen, false); // we'll call resize with the right capacity
-
-	this->size = binaryStrLen;
-
-	// Check for binary values
-	for (int i = 0; i < binaryStrLen; i++)
-		if (i_binaryStr[i] != '0' && i_binaryStr[i] != '1')
-			return NotBinaryValue;
-
-	const char* ptr = i_binaryStr;
-	size_t* ptr_storage = this->storage;
-	size_t loc = '\0', i = 0;
-	size_t bytes = binaryStrLen / 8;
-	size_t bits = binaryStrLen % 8;
-
-	for (size_t i = 0; i < bytes; i++)
-	{
-		for (int j = 7; j >= 0; j--)
-		{
-			loc = loc << 1; // built the number, shift left by 1
-			if (ptr[8*i + j] - '0') // this is '1'
-				loc |= 1;
-			else            // this is '0'
-				loc &= ~1;
-		}
-		*ptr_storage = loc;
-		ptr_storage++;
-		loc = '\0';
-	}
-	if (bits) {
-		for (int j = bits-1; j >= 0; j--)
-		{
-			loc = loc << 1; // built the number, shift left by 1
-			if (ptr[8 * bytes + j] - '0') // this is '1'
-				loc |= 1;
-			else            // this is '0'
-				loc &= ~1;
-		}
-		*ptr_storage = loc;
-	}
-	return 0;
-}
-
-size_t BitArray::operator[](size_t index) const {
-	return this->GetAt(index);
-}
-
-size_t BitArray::compare(const BitArray& other) const{
-	if (this->size != other.size)
-		return SizeNotMatch;
-
-	int i = this->size;
-	int j = 0;
-	while (i > 7) {
-		if (this->storage[j] - other.storage[j] != 0) // if different than 0 (1-1 = 0-0 = 0)
-			return 1;
-		i -= 8;
-		j++;
-	}
-	if (i) { // remain some bits
-	for (i = this->size % 8; i>0; i--)
-		if ((this->storage[j] >> i) - (other.storage[j] >> i) != 0) // if different than 0 (1-1 = 0-0 = 0)
-			return 1;
-	}
-
-	return 0;
+	return (this->storage[bytes] >> (bits - 1)) & 1; // take the right bytes, shift right, and mask with 1 (to get the exact bit)
 }
 
--- BitArray.h
+++ BitArray.h
@@ -14,14 +14,6 @@ public:
 	BitArray(const BitArray& copy);
 	~BitArray();
 
-	size_t getSize() const;
-	size_t GetAt(size_t index) const;
 	void SetAt(size_t index, bool value);
-
-	size_t ToBinaryStr(char* o_binaryStr, size_t binaryStrSize) const;
-	size_t FromBinaryStr(const char* i_binaryStr, size_t binaryStrLen);
-	
-	size_t operator[](size_t index) const;
-
-	size_t compare(const BitArray& other) const;
+	bool GetAt(size_t index) const;
 };
