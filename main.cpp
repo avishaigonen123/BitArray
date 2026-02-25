07136f4b2010858b01c27737294e56c384799bc2
avishaigonen123 <avishaigonen@gmail.com>
2026-02-25 15:41:14 +0200

    finished binaryIO

--- main.cpp
+++ main.cpp
@@ -2,27 +2,6 @@
 #include "stdio.h"
 
 int main() {
-	BitArray *bitarray = new BitArray();
-	for (int i = 0; i < 20; i++)
-	{
-		bitarray->SetAt(i, 0);
-		printf("%d", bitarray->GetAt(i));
-	}
-	printf("\n");
-
-	for (int i = 0; i < 10; i++)
-	{
-		bitarray->SetAt(i, 1);
-		printf("%d", bitarray->GetAt(i));
-	}
-	printf("\n");
-	//bitarray->SetAt(5, 1);
-	bitarray->SetAt(5, 0);
-	//bitarray->SetAt(16, 1);
-	for (int i = 0; i < 34; i++)
-	{
-		printf("%d", bitarray->GetAt(i));
-	}
-	printf("\n");
+	
 	return 0;
 }
