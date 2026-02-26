7a39c871273cf1f11d76de1d0e26bf79dd8e4a5c
avishaigonen123 <avishaigonen@gmail.com>
2026-02-26 13:09:47 +0200

    fixed class creation

--- BitArray.h
+++ BitArray.h
@@ -2,17 +2,19 @@
 
 class BitArray {
 private:
-	char* storage;
-	size_t size;
+	size_t* storage; // holds bytes, unsigned ints
+	size_t size;	 // how much bits
+	size_t capacity; // how much bytes
+
+
+	void resize(size_t new_capacity); // private method for resize the capacity
 
 public:
 	BitArray();
 	BitArray(const BitArray& copy);
 	~BitArray();
 
-	void SetAt(size_t index, bool value);
-	bool GetAt(size_t index) const;
+	size_t getSize() const;
 
-	bool ToBinaryStr(char* o_binaryStr, size_t binaryStrSize) const;
-	bool FromBinaryStr(const char* i_binaryStr, size_t binaryStrLen);
+	
 };
