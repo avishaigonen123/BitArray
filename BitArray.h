279ee1e28844639b66adae51f8cf0a3f495c7bf1
avishaigonen123 <avishaigonen@gmail.com>
2026-03-01 10:33:00 +0200

    added comments

--- BitArray.h
+++ BitArray.h
@@ -1,22 +1,42 @@
 # ifndef BITARRAY_H
 # define BITARRAY_H
 
-// The class BitArray implement array of bits, by storing 8 bits on each byte. 
-// It provides set of functions that gives various functionallity to dealing with the array.
+/**
+ * @class BitArray
+ * @brief Implements array of bits, by storing 8 bits on each byte. Provides set of functions for operations.
+ */
 class BitArray {
 private:
-	size_t* m_storage; // holds bytes, unsigned ints
-	size_t m_size;	 // how much bits
-	size_t m_capacity; // how much bytes
+	size_t* m_storage; 
+	size_t m_size_bits;	 
+	size_t m_capacity_bytes;
 
-
-	void resize(size_t new_capacity); // private method for resize the capacity
+	/**
+	 * @brief internal method for scaling the capacity of the storage. Can't shrink the capacity, will fail if trying to do so.
+	 * @param new_capacity 
+	*/
+	void scale(size_t new_capacity);
 
 public:
+	/**
+	 * @brief constructor, creates storage with capacity of 1 byte, and empty values.
+	*/
 	BitArray();
+
+	/**
+	 * @brief copy ctor, deep copy of all fields.
+	*/
 	BitArray(const BitArray& copy);
+
+	/**
+	 * @brief dtor, free the storage allocated memory
+	*/
 	~BitArray();
 
+	/**
+	 * @brief method that returns the actual size of bits
+	 * @return the field m_size_bits
+	*/
 	size_t getSize() const;
 };
 
