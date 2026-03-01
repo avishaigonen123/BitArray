757c946b9af907988940bdd18e028c545c45e864
avishaigonen123 <avishaigonen@gmail.com>
2026-03-01 10:33:00 +0200

    added comments

--- BitArray.cpp
+++ BitArray.cpp
@@ -5,25 +5,26 @@
 #include <stdlib.h>
 
 BitArray::BitArray() {
-	m_capacity = 0;
-	m_size = 0;
+	m_capacity_bytes = 0;
+	m_size_bits = 0;
 	m_storage = NULL;
-	resize(1);
+	// scale the storage to 1 byte capacity
+	scale(1); 
 }
 
 BitArray::BitArray(const BitArray& copy) {
 	free(m_storage);
 	this->m_storage = NULL;
 
-	this->m_size = copy.m_size;
-	this->m_capacity = copy.m_capacity;
-	this->m_storage = new size_t[this->m_capacity];
-	this->m_storage = (size_t*)malloc(this->m_capacity * sizeof(size_t));
+	this->m_size_bits = copy.m_size_bits;
+	this->m_capacity_bytes = copy.m_capacity_bytes;
+	this->m_storage = (size_t*)malloc(this->m_capacity_bytes * sizeof(size_t));
 	if (!this->m_storage) {
 		printf("Error memory allocation\n");
 		return;
 	}
-	for (size_t i = 0; i < this->m_capacity; i++)
+	// deep copy each byte block
+	for (size_t i = 0; i < this->m_capacity_bytes; i++)
 		this->m_storage[i] = copy.m_storage[i];
 }
 
@@ -32,38 +33,37 @@ BitArray::~BitArray() {
 	this->m_storage = NULL;
 }
 
-size_t BitArray::getSize() const
-{
-	return this->m_size;
+size_t BitArray::getSize() const {
+	return this->m_size_bits;
 }
 
-void BitArray::resize(size_t new_capacity) {
+void BitArray::scale(size_t new_capacity) {
 	// we can also simply use realloc, not forget to free the memory of the first parameter if realloc fails.
 	//size_t* tmp = (size_t*)realloc(this->m_storage, new_capacity * sizeof(size_t));
 
 	// Becuase I've already implemented this resize, I'll leave it as is.
 	// In realloc there is also functionly for shrinking the size, I don't have it becuase the resize is internal helper function, and I don't need shrinking.
-
 	size_t* tmp = this->m_storage;
 	size_t i = 0;
 
-	if (new_capacity < m_capacity) // resizing can't work
+	if (new_capacity < m_capacity_bytes) // only scaling, not shrinking
 		return;
 
+	// I want to reset with zero values, all blocks
 	this->m_storage = (size_t*)calloc(new_capacity, sizeof(size_t));
 	if (!this->m_storage) {
 		printf("Error memory allocation\n");
 		return;
 	}
-	for (; i < m_capacity; i++)
-	{
+	// copy bytes for old storage, new bytes will be already 0
+	for (; i < m_capacity_bytes; i++) {
 		if (!tmp)
 			this->m_storage[i] = NULL;
 		else
 			this->m_storage[i] = tmp[i];
-
 	}
-	m_capacity = new_capacity;
+	// set new capacity
+	m_capacity_bytes = new_capacity;
 
 	free(tmp);
 	tmp = NULL;
